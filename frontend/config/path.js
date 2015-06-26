e7a66856137e6a2e6bd1d55c932af5bd931a4ce7
Victor Sandoval <victor.sandoval@orbis.pe>
2015-06-26 12:49:03 -0500

    style(path): cambio de variable path a mayuscula

--- frontend/config/path.js
+++ frontend/config/path.js
@@ -5,31 +5,31 @@
  * @author Victor Sandoval
  */
 
-var path = {base : {}, frontend : {}, dest : {}};
+var Path = {base : {}, frontend : {}, dest : {}};
 
 /**
  * Ruta base del proyecto
  * @property base
  * @type String
  */
-path.base                         = __dirname + '/../..';
+Path.base                         = __dirname + '/../..';
 
 /**
  * Ruta base de la carpeta frontend
  * @property base
  * @type String
  */
-path.frontend.base                = path.base + '/frontend';
+Path.frontend.base                = Path.base + '/frontend';
 
 //-----------------------------------------------------------------------
-path.frontend.config              = path.frontend.base + '/config';
-path.frontend.coffee              = path.frontend.base + '/__coffee';
-path.frontend.jade                = path.frontend.base + '/__jade';
-path.frontend.stylus              = path.frontend.base + '/__stylus';
-path.frontend.staticFiles         = path.frontend.base + '/static_files';
-  path.frontend.staticFiles_fonts = path.frontend.staticFiles + '/fonts';
-  path.frontend.staticFiles_icons = path.frontend.staticFiles + '/icons';
-  path.frontend.staticFiles_img   = path.frontend.staticFiles + '/img';
+Path.frontend.config              = Path.frontend.base + '/config';
+Path.frontend.coffee              = Path.frontend.base + '/__coffee';
+Path.frontend.jade                = Path.frontend.base + '/__jade';
+Path.frontend.stylus              = Path.frontend.base + '/__stylus';
+Path.frontend.staticFiles         = Path.frontend.base + '/static_files';
+  Path.frontend.staticFiles_fonts = Path.frontend.staticFiles + '/fonts';
+  Path.frontend.staticFiles_icons = Path.frontend.staticFiles + '/icons';
+  Path.frontend.staticFiles_img   = Path.frontend.staticFiles + '/img';
 //-----------------------------------------------------------------------
 
 /**
@@ -37,21 +37,21 @@ path.frontend.staticFiles         = path.frontend.base + '/static_files';
  * @property serverFiles
  * @type String
  */
-path.dest.serverFiles             = path.base + '/public';
+Path.dest.serverFiles             = Path.base + '/public';
 
 /**
  * Ruta de los archivos estáticos (js, css, imagenes y fuentes)
  * @property serverFiles
  * @type String
  */
-path.dest.static                  = '';
+Path.dest.static                  = '';
 
 //-----------------------------------------------------------------------
-path.dest.css                     = path.dest.serverFiles + path.dest.static + '/css';
-path.dest.js                      = path.dest.serverFiles + path.dest.static + '/js';
-path.dest.img                     = path.dest.serverFiles + path.dest.static + '/img';
-path.dest.fonts                   = path.dest.serverFiles + path.dest.static + '/fonts';
-path.dest.html                    = path.dest.serverFiles + path.dest.static + '/html';
+Path.dest.css                     = Path.dest.serverFiles + Path.dest.static + '/css';
+Path.dest.js                      = Path.dest.serverFiles + Path.dest.static + '/js';
+Path.dest.img                     = Path.dest.serverFiles + Path.dest.static + '/img';
+Path.dest.fonts                   = Path.dest.serverFiles + Path.dest.static + '/fonts';
+Path.dest.html                    = Path.dest.serverFiles + Path.dest.static + '/html';
 //-----------------------------------------------------------------------
 
-module.exports = path;
+module.exports = Path;
