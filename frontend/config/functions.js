6dedd0cdc9b0a1110f2e240128d6745bb05af62a
Carlos Huamaní <carloshs92@gmail.com>
2015-08-04 12:34:27 -0500

    fix(tasks): Actualización de tareas Las tareas de de sprites y mocha son actualizadas con la tarea gulpSequence en vez de runSequence

--- frontend/config/functions.js
+++ frontend/config/functions.js
@@ -29,46 +29,6 @@ var Functions = {
       title   : 'Compilación completa',
       message : 'Tarea terminada'
     });
-  },
-  getFilesFrom : function(directory){
-    var fs = require('fs');
-    var path = require('path');
-    var files = [];
-    var getFiles = function(obj, dir){
-      fs.readdirSync(dir).forEach(function(file){
-        if (fs.statSync(path.join(dir, file)).isDirectory()){
-          obj[file] = {};
-          getFiles(obj[file], path.join(dir, file));               
-        } else if (/\.js$/.test(file)) {
-          file = path.basename(file, '.js');
-          files.push(path.join(dir, file)+'.js');
-        }
-      });
-      return files;
-    };
-    return getFiles({}, directory)
-  },
-  getPathJS: function(path, all, ubication){
-      var finalPath = path.dest.js + '/modules/';
-      if(!all){
-          finalPath = finalPath + ubication.join('/') + '/' ;
-      }
-      return finalPath;
-  },
-  getPathTests: function(path, all, ubication){
-      var basePath = path.frontend.tests + '/mocha/';
-      if(!all){
-          basePath = basePath + ubication.join('/') + '/' ;
-      }
-      finalPath = [basePath + '*.js', basePath + '**/*.js'];
-      return finalPath;
-  },
-  tellMeWhatIamTesting:  function(all, ubication){
-      if(!all){
-          console.log('=> Se ejecutan las pruebas de ' + ubication.join('/'));
-      }else{
-          console.log('=> Se ejecutan todas las pruebas');
-      }
   }
 }
 
--- frontend/tests/mocha/postulante/registro/paso2/test_selection_modal.js
+++ frontend/tests/mocha/postulante/registro/paso2/test_selection_modal.js
@@ -5,7 +5,7 @@ describe('test_selection_modal', function(){
 
 	describe("eSearch", function () {
 		before(function () {
-			modulo = yOSON.AppCore.getModuleFunctions('selection_modal');
+			//modulo = yOSON.AppCore.getModuleFunctions('selection_modal');
 			//$('body').append('<ul data-key="areas" class="filters"> <li> <form id="fFilterAC" data-type="areas"> <ul> <li class="filter_options"> <input data-value="legal" class="checkN" type="checkbox" checked="checked"> </li><li class="filter_options"> <input data-value="operaciones" class="checkN" type="checkbox" checked="checked"> </li><li class="filter_options"> <input data-value="mantenimiento" class="checkN" type="checkbox"> </li></ul> </form> </li></ul> <ul data-key="nivel" class="filters"> <li> <form id="fFilterSNiv" data-type="nivel"> <ul> <li class="filter_options"> <input data-value="gerencia" class="checkN" type="checkbox"> </li><li class="filter_options"> <input data-value="jefe-supervisor" class="checkN" type="checkbox" checked="checked"> </li></ul> </form> </li></ul> <ul data-key="ubicacion" class="filters"> <li> <form id="fFilterSNiv" data-type="ubicacion"> <ul> <li class="filter_options"> <input data-value="arequipa" class="checkN" type="checkbox"> </li></ul> </form> </li></ul>');
 		})
 
