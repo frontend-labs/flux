d280618924969683a4789952a96bf78ab19eec74
Victor Sandoval <victor.sandoval@orbis.pe>
2015-08-07 11:51:17 -0500

    styles(tasks): se cambian tabs por espacios

--- frontend/config/tasks/gulp_fonts.js
+++ frontend/config/tasks/gulp_fonts.js
@@ -13,25 +13,25 @@ var fs   = require('fs');
 
 function Task(gulp, path, config, plugins, functions){
 
-	/**
-	 * Tarea para generar archivo .styl de las fuentes
-	 * (gulp fonts:compile)
-	 */
-	gulp.task('fonts:compile', function(cb){
-		fs.readdir(path.frontend.fonts + "/", function(err, files) {
-			if (err) throw err;
-			var dirList = files.filter(function(file) { return (/^[^_]*$/g).test(file); });
-			gulp.src(path.frontend.fonts + '/_template/fonts.styl')
-				.pipe(plugins.consolidate('lodash', { dirList: dirList }))
-				.pipe(gulp.dest(path.frontend.pre_css + '/layout'));
-		})
-	});
+  /**
+   * Tarea para generar archivo .styl de las fuentes
+   * (gulp fonts:compile)
+   */
+  gulp.task('fonts:compile', function(cb){
+    fs.readdir(path.frontend.fonts + "/", function(err, files) {
+      if (err) throw err;
+      var dirList = files.filter(function(file) { return (/^[^_]*$/g).test(file); });
+      gulp.src(path.frontend.fonts + '/_template/fonts.styl')
+        .pipe(plugins.consolidate('lodash', { dirList: dirList }))
+        .pipe(gulp.dest(path.frontend.pre_css + '/layout'));
+    })
+  });
 
-	/**
-	 * Tarea principal
-	 * (gulp fonts)
-	 */
-	gulp.task('fonts', plugins.gulpSequence('fonts:compile', 'css', 'copy:fonts'));
+  /**
+   * Tarea principal
+   * (gulp fonts)
+   */
+  gulp.task('fonts', plugins.gulpSequence('fonts:compile', 'css', 'copy:fonts'));
 
 }
 
